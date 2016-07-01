0ba8229930f617d479b67a0ac8f90fd07a107db0
Alex <alexaraujony@gmail.com>
2016-07-01 16:35:10 -0300

    comit projeto

--- src/main/java/br/com/alex/lojavirtual/LoginActivity.java
+++ src/main/java/br/com/alex/lojavirtual/LoginActivity.java
@@ -2,8 +2,15 @@ package br.com.alex.lojavirtual;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtUsuario, edtSenha;
+    private Button btnLogar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+
+        edtUsuario = (EditText) findViewById(R.id.edtUsuario);
+        edtSenha = (EditText) findViewById(R.id.edtSenha);
     }
 }
